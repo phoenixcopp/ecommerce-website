b3fe47dacc2415f36e41ae1b8548b75e70b61dec
phoenixcopp <phoenixcopp@yahoo.com>
2023-02-08 14:43:33 -0800

    cart finished

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 
-export const Footer = () => {
+const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer-container'>
+      <p> 2023 Mechanical Keyboards all rights reserved </p>
+      <p className='icons'> 
+        <AiFillInstagram/>
+        <AiOutlineTwitter/>
+      </p>
+    </div>
   )
 }
+
+export default Footer;
--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -12,7 +12,7 @@ const HeroBanner = ({heroBanner}) => {
         <h1>{heroBanner.largeText1}</h1>
         <img src={urlFor(heroBanner.image)} alt="keyboard"  className='hero-banner-image'/>
         <div>
-          <Link href={'/product/${heroBanner.product}'}>
+          <Link href={`/product/${heroBanner.product}`}>
             <button type="button"> {heroBanner.buttonText} </button>
           </Link>
           <div className='desc'>
