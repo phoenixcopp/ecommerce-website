fd6530ba2031d43880cd08e7ceeda19a4a1efd6d
phoenixcopp <phoenixcopp@yahoo.com>
2023-02-08 16:49:41 -0800

    success page implemented

--- pages/success.js
+++ pages/success.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Link from 'next/link';
+import { BsBagCheckFill } from 'react-icons/bs';
+import { useStateContext } from '@/context/StateContext';
+import { runConfetti } from '@/lib/utils';
 
 const success = () => {
-  return (
-    <div>success</div>
-  )
+    const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+  
+    useEffect(() => {
+        localStorage.clear();
+        setCartItems([]);
+        setTotalPrice(0);
+        setTotalQuantities(0);
+        runConfetti();
+    }, []);
+
+
+    return (
+        <div className='success-wrapper'>
+            <div className='success'>
+                <p className='icon'>
+                    <BsBagCheckFill/>
+                </p>
+                <h2> Thank you for your purchase!</h2>
+                <p className='email-msg'> Check your email inbox for the reciept </p>
+                <p className='description'>
+                    If you have any questions please email
+                    <a className='email' href='mailto:notarealemail@example.com'>notarealemail@example.com</a>
+                </p>
+                <Link href='/'>
+                    <button type='button' width='300px' className='btn'>
+                        CONTINUR SHOPPING
+                    </button>
+                </Link>
+            </div>
+
+        </div>
+    )
 }
 
 export default success
