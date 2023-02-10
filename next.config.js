a61c5220f2a2304762c5e44ee650f6ecd293d6c1
phoenixcopp <phoenixcopp@yahoo.com>
2023-02-09 16:52:33 -0800

    changed sanity client version

--- next.config.js
+++ next.config.js
@@ -3,4 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+	images: {
+		domains: ['cdn.sanity.io'],
+		loader: 'custom'
+	},
+  nextConfig
+};
